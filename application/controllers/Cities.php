1e8f55982b29845a1ded9bd621256694f7a6d7d2
cmurra <murrarts@gmail.com>
2016-08-31 16:15:04 -0500

    proyecto on htacces corregido

---
+++ application/controllers/Cities.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'libraries/REST_Controller.php';
+
+class Welcome extends REST_Controller {
+	
+	public function index_get(){
+
+	}
+	public function find_get($id){
+			
+	}
+	public function index_post(){
+		
+	}
+	public function index_put(){
+		
+	}
+	public function index_delete(){
+		
+	}
+}
